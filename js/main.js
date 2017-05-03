149eee4ad4ca6ce61f51ade1ecebba7f0ac0e815
Lisa Long <garrisonridge@gmail.com>
2017-05-03 19:35:17 -0400

    Adjust mmenu and header areas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 
 $(document).ready(function() {
 
-//for fancybox
-$('.fancybox').fancybox({
-openEffect: 'elastic',
-closeEffect: 'elastic',
-helpers: {
-media: {}
-}
-});
+    //mmenu
+        $('#menu').mmenu({
+        // options
+        // preventDefault is needed because href points to # on same page
+        // if menu linked to other pages, would not need to set
+        // preventDefault to false
+        //close: true needed because when did above, only, menu stayed open
+            onClick: {
+                close: true,
+                preventDefault: false
+            }
+        }, {
+
+        // configuration
+            offCanvas: {
+                pageSelector: '#wrapper'
+            }
+        });
+
+        var mainMenu = $('#menu').data('mmenu');
+
+        $('#menu-button').click(function () {
+            mainMenu.open();
+        });
+
+        //for fancybox
+        $('.fancybox').fancybox({
+            openEffect: 'elastic',
+            closeEffect: 'elastic',
+            helpers: {
+                media: {}
+            }
+        });
 
 
 });
